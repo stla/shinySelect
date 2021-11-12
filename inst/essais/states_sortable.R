2a3a51d02cee72dfe46a83169beefaeac8979761
stla <laurent_step@yahoo.fr>
2021-11-12 16:27:12 +0100

    styles

--- inst/essais/states_sortable.R
+++ inst/essais/states_sortable.R
@@ -4,11 +4,19 @@ library(bslib)
 library(fontawesome)
 
 states <- HTMLgroupedChoices(
-  groups = lapply(list("East Coast", "West Coast", "Midwest"), tags$h2),
+  groups = lapply(list("East Coast", "West Coast", "Midwest"), function(x){
+    tags$h2(x, style="text-decoration: underline")
+  }),
   labels = list(
-    lapply(list("NY", "NJ", "CT"), function(x) tags$span(x, style="color:red")),
-    lapply(list("WA", "OR", "CA"), function(x) tags$span(x, style="color:green")),
-    lapply(list("MN", "WI", "IA"), function(x) tags$span(x, style="color:blue"))
+    lapply(list("NY", "NJ", "CT"), function(x){
+      tags$span(HTML("&bull;"), x, style="color: red")
+    }),
+    lapply(list("WA", "OR", "CA"), function(x){
+      tags$span(HTML("&bull;"), x, style="color: green")
+    }),
+    lapply(list("MN", "WI", "IA"), function(x){
+      tags$span(HTML("&bull;"), x, style="color: blue")
+    })
   ),
   values = list(
     list("NY", "NJ", "CT"),
@@ -19,18 +27,27 @@ states <- HTMLgroupedChoices(
 
 
 styles <- list(
-  borderBottom = "5px solid orange",
-  color = list(selected = "lime", otherwise = "pink"),
-  backgroundColor = list(selected = "cyan", otherwise = "seashell")
+  borderBottom = "2px dotted orange",
+  backgroundColor = list(
+    selected = "cyan",
+    focused = "lemonchiffon",
+    otherwise = "seashell"
+  )
 )
 controlStyles = list(
-  marginTop = "0"
+  marginTop = "0",
+  boxShadow = toString(c(
+    "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px",
+    "rgba(0, 0, 0, 0.3) 0px 30px 60px -30px",
+    "rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;"
+  ))
 )
 multiValueStyles = list(
-  backgroundColor = "orange"
+  backgroundColor = "lavenderblush"
 )
 multiValueLabelStyles = list(
-  fontStyle = "italic"
+  fontStyle = "italic",
+  fontWeight = "bold"
 )
 multiValueRemoveStyles = list(
   color = "hotpink",
@@ -40,19 +57,19 @@ multiValueRemoveStyles = list(
   )
 )
 
-# CSS <- '
-# div[class$="-menu"][id^="react-select"] {position: static}
-# div[class$="-control"] + div {position: static}'
+CSS <- '
+div[class$="-group"][id^="react-select"][id$="-heading"] {
+  background-color: #671227
+}'
 
 ui <- fluidPage(
   theme = bs_theme(version = 4),
-  # tags$head(
-  #   tags$style(HTML(CSS))
-  # ),
+  tags$head(
+    tags$style(HTML(CSS))
+  ),
   titlePanel("reactR Input Example"),
   selectControlInput(
     "inputid", label = tags$h1("Make a choice", style="color: red;"),
-    containerClass = NULL,
     optionsStyles = styles,
     controlStyles = controlStyles,
     multiValueStyles = multiValueStyles,
@@ -65,6 +82,7 @@ ui <- fluidPage(
     animated = TRUE,
     ignoreCaseOnFilter = FALSE
   ),
+  br(),
   verbatimTextOutput("textOutput"),
   actionButton("toggle", "Toggle menu")
 )
@@ -74,7 +92,7 @@ server <- function(input, output, session) {
     print(input$inputid)
   })
   output$textOutput <- renderPrint({
-    sprintf("You selected: %s", input$inputid)
+    sprintf("You selected: %s", toString(input$inputid))
   })
   observeEvent(input[["toggle"]], {
     toggleMenu(session, "inputid")
